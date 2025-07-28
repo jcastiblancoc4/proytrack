982c0098f7cb7a027b8616deb4036e906295063b
jcastiblancoc4 <57145816+jcastiblancoc4@users.noreply.github.com>
2025-07-27 19:07:03 -0500

    Update manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,3 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
-//= link rails-ujs.js
-//= link tailwind.css
+//= link application.css
